d94d9f931feab3770bdf7c76096680fb4af18fa8
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-06-27 17:41:53 -0600

    added gradient

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/js/partials/Navbar";
+import Header from "./components/js/partials/Header";
 import Footer from "./components/js/partials/Footer";
 import Login from "./components/js/Login";
 import Store from "./components/js/Store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App(props) {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/">
-          <Store />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <div className="bg-gradient-1">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Store />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
