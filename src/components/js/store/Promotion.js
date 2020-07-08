7073e1b0aa449220cd6de8f0826b04b5d51884ad
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-07-07 20:15:11 -0600

    added basic modal

--- src/components/js/store/Promotion.js
+++ src/components/js/store/Promotion.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-modal";
 
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+//Modal.setAppElement("#promotion");
 const Promotion = (props) => {
+  const [show, _setShow] = useState(false);
+  const [code, _setCode] = useState("");
+
+  const handleClose = () => _setShow(false);
+  const handleShow = () => _setShow(true);
+
+  const onSubmit = () => {};
+
   return (
     <div id="promotion">
+      <Modal
+        id="promotion-modal"
+        className="w-50 m-auto d-flex align-items-center bg-white"
+        isOpen={show}
+        onRequestClose={handleClose}
+        contentLabel="Redeem Event"
+      >
+        <h2>Redeem Event</h2>
+        <input
+          type="text"
+          id="event-code"
+          value={code}
+          onChange={(event) => _setCode(event.target.value)}
+          placeholder="Enter event code"
+        ></input>
+        <button id="button-close" onClick={handleClose}>
+          Cancel
+        </button>
+        <button id="button-submit" onSubmit={onSubmit}>
+          Submit
+        </button>
+      </Modal>
       <a
         target="_blank"
         rel="noopener noreferrer"
@@ -15,7 +49,13 @@ const Promotion = (props) => {
         />
       </a>
       <div id="promotion-buttons">
-        <button className="w-100 m-sm-2 mb-sm-2 p-sm-3">Redeem Event</button>
+        <Button
+          variant="primary"
+          onClick={handleShow}
+          className="w-100 m-sm-2 mb-sm-2 p-sm-3"
+        >
+          Redeem Event
+        </Button>
         <a
           href={props.promotion["event_url"]}
           target="_blank"
