0740ae41ee94d572b9831e5d173564d758c85d23
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-07-08 19:55:55 -0600

    fixed user initialization

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext();
 
@@ -10,6 +10,19 @@ export default (props) => {
     setUserData(newUserData);
     localStorage.setItem("userData", JSON.stringify(newUserData));
   };
+
+  const initUserData = () => {
+    console.log(userData);
+    if (userData) {
+      fetch(`https://api.youthcomputing.ca/users/${userData["id"]}`)
+        .then((response) => response.json())
+        .then((response) => {
+          updateUserData(response["userData"]);
+        });
+    }
+  };
+  useEffect(initUserData, []);
+
   return (
     <UserContext.Provider value={{ userData, updateUserData }}>
       {props.children}
