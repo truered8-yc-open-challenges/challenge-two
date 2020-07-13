77cddfb3461a35d3f41b94821e73fd98b3bf7b9f
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-07-13 15:05:38 -0600

    moved "redeem event" to account

--- src/components/js/Store.js
+++ src/components/js/Store.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../css/Store.css";
-import Button from "react-bootstrap/Button";
 import Promotion from "./store/Promotion";
-import EventModal from "./store/EventModal";
 
 const getRandomElement = (list) => {
   return list[Math.floor(Math.random() * list.length)];
@@ -10,10 +8,6 @@ const getRandomElement = (list) => {
 
 const Store = () => {
   const [promotion, _setPromotion] = useState(null);
-
-  const [showEvent, _setShowEvent] = useState(false);
-  const handleClose = () => _setShowEvent(false);
-  const handleShow = () => _setShowEvent(true);
 
   useEffect(() => {
     const getPromotion = async () => {
@@ -28,23 +22,7 @@ const Store = () => {
       _setPromotion(response);
     });
   }, []);
-  return (
-    <div id="store">
-      <EventModal
-        show={showEvent}
-        handleClose={handleClose}
-        handleShow={handleShow}
-      />
-      {promotion}
-      <Button
-        variant="primary"
-        onClick={handleShow}
-        className="w-75 m-sm-2 mb-sm-2 p-sm-3"
-      >
-        Redeem Event
-      </Button>
-    </div>
-  );
+  return <div id="store">{promotion}</div>;
 };
 
 export default Store;
--- src/components/js/partials/Account.js
+++ src/components/js/partials/Account.js
@@ -1,18 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import Button from "react-bootstrap/Button";
+import EventModal from "./../store/EventModal";
 import { UserContext } from "./../../../contexts/UserContext";
 
 import "../../css/partials.css";
 
 const Account = (props) => {
+  const [showEvent, _setShowEvent] = useState(false);
+  const handleClose = () => _setShowEvent(false);
+  const handleShow = () => _setShowEvent(true);
+
   const { userData } = useContext(UserContext);
 
   if (userData) {
     return (
       <div id="account" className="w-25 p-sm-2 bg-white shadow">
-        Hi {userData["name"]}!
-        <button id="points" className="p-sm-1 rounded-circle">
+        <EventModal
+          show={showEvent}
+          handleClose={handleClose}
+          handleShow={handleShow}
+        />
+        <Button
+          variant="primary"
+          onClick={handleShow}
+          className="w-100 border-0 shadow-none m-0 p-0 bg-white text-black-50"
+        >
+          Redeem Event
+        </Button>
+        <Button id="points" className="p-sm-1 rounded-circle">
           {userData["points"]}
-        </button>
+        </Button>
       </div>
     );
   }
