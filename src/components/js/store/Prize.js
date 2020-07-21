a93e7cd57e0de3c31490d059ab57a8739c3fdd79
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-07-21 16:31:45 -0600

    made each prize a button

--- src/components/js/store/Prize.js
+++ src/components/js/store/Prize.js
@@ -3,21 +3,21 @@ import Button from "react-bootstrap/Button";
 
 const Prize = (props) => {
   return (
-    <div className="prize w-25 p-sm-1 m-0 d-inline-block bg-white">
-      <img
-        src={props.prizeJson["image_url"]}
-        alt={props.prizeJson["name"]}
-        className="w-100"
-      />
-      <div className="p-sm-2">
-        <strong>{props.prizeJson["name"]}</strong>
-      </div>
-      <div className="float-left m-sm-2">
-        <Button>Redeem Now</Button>
-      </div>
-      <div className="float-right m-sm-2">
-        {props.prizeJson["points"]} points
-      </div>
+    <div className="prize w-25 p-sm-1 m-0 d-inline-block rounded-lg">
+      <Button className="bg-white text-body">
+        <img
+          src={props.prizeJson["image_url"]}
+          alt={props.prizeJson["name"]}
+          className="w-100"
+        />
+        <div className="p-sm-2">
+          <strong>{props.prizeJson["name"]}</strong>
+        </div>
+        <div className="float-left m-sm-2">Redeem Now</div>
+        <div className="float-right m-sm-2">
+          {props.prizeJson["points"]} points
+        </div>
+      </Button>
     </div>
   );
 };
