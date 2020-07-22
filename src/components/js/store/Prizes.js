648a10eeda8ad28d60803bf1f8f5c3787d6c60da
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-07-22 12:21:57 -0600

    added query reset

--- src/components/js/store/Prizes.js
+++ src/components/js/store/Prizes.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import PrizeCategories from "./PrizeCategories";
 import PrizeList from "./PrizeList";
 
+import { SearchContext } from "./../../../contexts/SearchContext";
+
 const Prizes = (props) => {
   const [category, _setCategory] = useState("Laptops");
+  const { setQuery } = useContext(SearchContext);
+  const updateCategory = (newCategory) => {
+    _setCategory(newCategory);
+    setQuery("");
+  };
 
   return (
     <div id="prizes">
-      <PrizeCategories category={category} setCategory={_setCategory} />
+      <PrizeCategories category={category} setCategory={updateCategory} />
       <PrizeList prizeList={props.prizeList} category={category} />
     </div>
   );
