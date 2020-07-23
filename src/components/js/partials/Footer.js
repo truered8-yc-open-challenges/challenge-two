aea47d1e6cc76c78f3cafb4c323daf8d7fc100b0
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2020-07-22 23:57:34 -0600

    fixed footer

--- src/components/js/partials/Footer.js
+++ src/components/js/partials/Footer.js
@@ -3,7 +3,7 @@ import "../../css/partials.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="text-right p-sm-3 mr-sm-2">
       <p>&copy; YouthComputing 2020</p>
     </footer>
   );
